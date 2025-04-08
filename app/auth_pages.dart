b7ff2c92e4e57b6b35007cae8c8f2725dea95d22
brennanrobert05 <66694910+brennanrobert05@users.noreply.github.com>
2025-04-08 15:33:55 +0100

    Create auth_pages.dart

---
+++ app/auth_pages.dart
@@ -0,0 +1,29 @@
+// Determining whether the login details match up with the details on Firebase Authentication
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'login_or_register_page.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // user is logged in
+          if (snapshot.hasData) {
+            return HomePage();
+          }
+
+          // user is NOT logged in
+          else {
+            return LoginOrRegisterPage();
+          }
+        },
+      ),
+    );
+  }
+}
